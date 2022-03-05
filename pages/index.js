fa9d979afc4ad790da214826fa7f302c6a2bc45c
Nitin Rana <nitinranagraphian@gmail.com>
2022-03-05 12:01:08 +0530

    df

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import LoginOrSignUp from '../component/loginOrSignUp';
 import {auth} from '../fireb/firebApp';
 export default function Home() {
   
@@ -14,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <LoginOrSignUp/>
+        
       </main>
 
     </div>
